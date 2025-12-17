da62bde3beded114042b26ceedbcefa7e555e4e6
ahadji05 <andreashadji1995@gmail.com>
2025-12-17 13:30:54 +0100

    added default valus for the Item member-variables to identify if an item is not initialized.

--- Item.hpp
+++ Item.hpp
@@ -12,20 +12,32 @@ template< typename KeyType >
 class Item {
 
     protected:
-        KeyType m_itemID;
-        std::string m_kind;
+        KeyType     m_itemID = UINT64_MAX;
+        std::string m_kind   = "UDEF";
 
     public:
+        using key_type = KeyType;
+
         virtual ~Item(){}
         Item(){}
         Item( KeyType id, std::string kind ) : m_itemID( id ), m_kind( kind ) {}
-        KeyType getID() const             { return m_itemID; }
-        void    setID( KeyType const& id ){ m_itemID = id;   }
-        std::string getKind() const       { return m_kind;  }
-        void        setKind( std::string const& kind ){ m_kind = kind; }
+
+        KeyType getID() const {
+            if ( m_itemID == UINT64_MAX )
+                throw std::runtime_error( "UNDEFINED: m_itemID" );
+            return m_itemID;
+        }
+
+        std::string getKind() const {
+            if ( m_kind.empty() || m_kind == "UDEF" )
+                throw std::runtime_error( "UNDEFINED: m_kind" );
+            return m_kind;
+        }
+
+        void setID( KeyType const& id ){ m_itemID = id;   }
+        void setKind( std::string const& kind ){ m_kind = kind; }
 
         // virtual public interface
-        virtual void   load()   = 0;               // load item from pool to cache.
-        virtual void   unload() = 0;               // unload from cache.
-        virtual void * get() const = 0;            // get read-only access.
+        virtual void load()   = 0; // load item from pool to cache.
+        virtual void unload() = 0; // unload from cache.
 };
