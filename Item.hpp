cf39b7cbba4ee30aac1aaab46ba662f79c178dcf
ahadji05 <andreashadji1995@gmail.com>
2025-12-17 14:25:57 +0100

    deprecate m_kind variable from abstract class Item<T>

--- Item.hpp
+++ Item.hpp
@@ -12,30 +12,15 @@ template< typename KeyType >
 class Item {
 
     protected:
-        KeyType     m_itemID = UINT64_MAX;
-        std::string m_kind   = "UDEF";
+        KeyType m_itemID;
 
     public:
         using key_type = KeyType;
 
         virtual ~Item(){}
         Item(){}
-        Item( KeyType id, std::string kind ) : m_itemID( id ), m_kind( kind ) {}
-
-        KeyType getID() const {
-            if ( m_itemID == UINT64_MAX )
-                throw std::runtime_error( "UNDEFINED: m_itemID" );
-            return m_itemID;
-        }
-
-        std::string getKind() const {
-            if ( m_kind.empty() || m_kind == "UDEF" )
-                throw std::runtime_error( "UNDEFINED: m_kind" );
-            return m_kind;
-        }
-
-        void setID( KeyType const& id ){ m_itemID = id;   }
-        void setKind( std::string const& kind ){ m_kind = kind; }
+        Item( KeyType id ) : m_itemID( id ) {}
+        KeyType getID() const { return m_itemID; }
 
         // virtual public interface
         virtual void load()   = 0; // load item from pool to cache.
